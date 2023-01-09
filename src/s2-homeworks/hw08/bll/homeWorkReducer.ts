04fc62cd39d204c6f6727874a523fb4fa049ffb1
acronisv <acronisv@gmail.com>
2023-01-09 17:20:39 +0300

    Add HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,19 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
     switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
+        case 'sort': {
+            let stateCopy = [...state]
+            if (action.payload == 'up') {
+                stateCopy.sort((a, b) => b.age - a.age)
+            } else if (action.payload == 'down') {
+                stateCopy.sort((a, b) => a.age - b.age)
+            }
+            return stateCopy
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(el => el.age >= action.payload)
         }
         default:
             return state
